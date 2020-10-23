e8a6b857e3df836e929db74062a31898c3ab9282
mitsyksandor <mitsyk@prefixbox.com>
2020-10-23 21:32:45 +0200

    client app api call with react hooks

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { Header, List } from 'semantic-ui-react';
+import axios from 'axios';
+
+import { IActivity } from '../models/activity';
+import NavBar from '../../feature/nav/navBar';
+
+const App = () => {
+    const [activities, setActivities] = useState<IActivity[]>([]);
+
+    useEffect(() =>{
+        axios.get<IActivity[]>('http://localhost:5000/api/activities')
+        .then(response => {
+            setActivities(response.data,)
+        });
+    }, []);
+
+    return (
+        <div>
+            <NavBar />
+            <List>
+                {
+                    activities.map((activity) => (
+                        <List.Item key={activity.id}>{activity.title}</List.Item>
+                    ))
+                }
+            </List>
+        </div>
+    );
+}
+
+export default App;
---
+++ client-app/src/feature/nav/navBar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button, Container, Menu } from 'semantic-ui-react';
+
+const NavBar = () => {
+    return (
+        <Menu fixed="top" inverted>
+            <Container>
+                <Menu.Item>
+                    <img src="/assets/logo.png" alt="logo" />
+                    Reactivities
+                </Menu.Item>
+                <Menu.Item name="Activities" />
+                <Menu.Item>
+                    <Button positive content="Create activity" />
+                </Menu.Item>
+            </Container>
+      </Menu>
+    )
+}
+
+export default NavBar;
