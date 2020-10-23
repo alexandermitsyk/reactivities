fe1c61252ff6d1989ebdefe03415929f935afb63
mitsyksandor <mitsyk@prefixbox.com>
2020-10-23 15:39:04 +0200

    client-app with cors policy added

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,6 +24,13 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -34,6 +41,12 @@ namespace API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors("CorsPolicy");
+
+            //app.UseEndpoints(endpoints => {
+            //    endpoints.MapControllers();
+            //});
 
             app.UseHttpsRedirection();
 
